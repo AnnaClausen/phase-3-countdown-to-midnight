365d7c0a132587e88bff57453f253e0bd4352c1a
Anna Clausen <anna.revall.clausen@gmail.com>
2022-09-11 19:13:15 -0700

    commit tree 4a584d5a24735f50d7a5250af12a09886e21ca71

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown(number=10)
+    while number >= 1
+        puts "#{number} SECOND(S)!"
+        sleep 1
+        number -= 1
+    end
+        "HAPPY NEW YEAR!"
 end
+
+
